2ba32fcaffb6d0d2f16ce85afe9120a3ede08861
D1KANI <46443553+D1KANI@users.noreply.github.com>
2020-02-19 12:45:36 +0400

    Changes to be committed: 	modified:   #source/scss/style.scss 	modified:   css/slick/slick.css 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   js/sctipt.js

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,21 +1,71 @@
 
 $('.footer__item__title').click(function(){
     $(this).parent('.footer__item').children('.footer__item_content').toggle(200)
-  });
+});
+
 
 $('.block-FAQ__question').click(function(){
     $(this).parent('.block-FAQ__item').children('.block-FAQ__answer').toggle(200);
     $(this).toggleClass('block-FAQ__question_active');
 });
 
-$(document).ready(function() {
-    $(window).resize(function() {
-               if ($(window).width() <= '1025'){
-                    $(document).ready(function(){
-                        $('.block-rewiews__row').slick({
-                        });
-
-                    });      
-                };
-            });
+function mobileOnlySlider() {
+    $(document).ready(function(){
+        $('.block-rewiews__row').slick({
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+            touchMove:true,
+            dots: false,
+                pauseOnHover: false,
+                responsive: [{
+                    breakpoint: 568,
+                    settings: {
+                        slidesToShow: 1,
+                        skiderToScroll: 1,
+                        settings:"unslick"
+                }
+            }]
         });
+    });
+}
+
+function mobileOnlySliderTop() {
+    $(document).ready(function(){
+        $('.block-advantage__row').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            touchMove:true,
+            dots: false,
+                pauseOnHover: false,
+                responsive: [{
+                    breakpoint: 568,
+                    settings: {
+                        slidesToShow: 1,
+                        skiderToScroll: 1,
+                        settings:"unslick"
+                }
+            }
+            ]
+        });
+    });
+}
+
+if(window.innerWidth < 1000) {
+    mobileOnlySlider();
+    mobileOnlySliderTop();
+}
+$(window).resize(function(e){
+    if(window.innerWidth < 1000) {
+        if(!$('.slider').hasClass('slick-initialized')){
+            mobileOnlySlider();
+            mobileOnlySliderTop();
+        }
+
+    }else{
+        if($('.slider').hasClass('slick-initialized')){
+            $('.slider').slick('unslick');
+        }
+    }
+});
