bbfb7634c88e35ec280608d3159595226ff400bf
D1KANI <46443553+D1KANI@users.noreply.github.com>
2020-02-20 15:27:05 +0400

    modified:   #source/scss/style.scss 	modified:   css/slick/slick-theme.css 	modified:   css/slick/slick.css 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   index.html 	modified:   js/sctipt.js

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,7 +1,10 @@
-
 $('.footer__item__title').click(function(){
-    $(this).parent('.footer__item').children('.footer__item_content').toggle(200)
+    if (innerWidth <= 700) {
+            $(this).parent('.footer__item').children('.footer__item_content').toggle(200);
+            $(this).parent('.footer__item').toggleClass('footer__item_active');
+    }   
 });
+
 
 
 $('.block-FAQ__question').click(function(){
@@ -13,13 +16,16 @@ function mobileOnlySlider() {
     $(document).ready(function(){
         $('.block-rewiews__row').slick({
             slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToScroll: 1,
             arrows: false,
             touchMove:true,
-            dots: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 700,
                 pauseOnHover: false,
                 responsive: [{
-                    breakpoint: 568,
+                    breakpoint: 620,
                     settings: {
                         slidesToShow: 1,
                         skiderToScroll: 1,
@@ -37,7 +43,7 @@ function mobileOnlySliderTop() {
             slidesToScroll: 1,
             arrows: false,
             touchMove:true,
-            dots: false,
+            dots: true,
                 pauseOnHover: false,
                 responsive: [{
                     breakpoint: 568,
@@ -54,12 +60,26 @@ function mobileOnlySliderTop() {
 
 if(window.innerWidth < 1000) {
     mobileOnlySlider();
-    mobileOnlySliderTop();
 }
 $(window).resize(function(e){
     if(window.innerWidth < 1000) {
         if(!$('.slider').hasClass('slick-initialized')){
             mobileOnlySlider();
+        }
+
+    }else{
+        if($('.slider').hasClass('slick-initialized')){
+            $('.slider').slick('unslick');
+        }
+    }
+});
+
+if(window.innerWidth < 1000) {
+    mobileOnlySliderTop();
+}
+$(window).resize(function(e){
+    if(window.innerWidth < 1000) {
+        if(!$('.slider').hasClass('slick-initialized')){
             mobileOnlySliderTop();
         }
 
@@ -68,4 +88,9 @@ $(window).resize(function(e){
             $('.slider').slick('unslick');
         }
     }
+});
+
+$('.burger').on('click', function() {
+    $('.poopup').fadeToggle()
+    $(this).toggleClass('active')
 });
